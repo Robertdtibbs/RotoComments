03f865f49a584cdd2c8db97ffbc650ed2c803d7b
Robert Tibbs <tibbsr09@gmail.com>
2019-06-17 08:01:28 -0400

    Was able to fix the scraping issue with the error for undefiend parent, app now scrapes articles, titles, links, and images to display on comment app.

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var morgan = require('morgan');
-var mongoose = require('mongoose');
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
 
-var axios = require('axios');
-var cheerio = require('cheerio');
+var axios = require("axios");
+var cheerio = require("cheerio");
 
 var db = require("./models");
 
@@ -11,77 +11,102 @@ var PORT = 3000;
 
 var app = express();
 
-app.use(morgan('dev'));
+app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
+// Connect to the Mongo DB
 mongoose.connect("mongodb://localhost/eplTalk", { useNewUrlParser: true });
 
+// Routes
 
-axios.get("https://www.premierleague.com/news").then(function(response) {
+// A GET route for scraping website
+app.get("/scrape", function(req, res) {
+  // Grab the html with axios
+  axios.get("https://www.premierleague.com/news").then(function(response) {
+    // Load that into cheerio and save it to our selector
+    var $ = cheerio.load(response.data);
 
-  // Load the HTML into cheerio and save it to a variable
-  // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
-  var $ = cheerio.load(response.data);
+    // Grab every section with the class of featuredArticle
+    $("section.featuredArticle").each(function(i, element) {
+      // Save an empty result object
+      var result = {};
 
-  // An empty array to save the data that we'll scrape
-  var results = [];
+      // Grab four different specific items on the webpage
+      result.title = $(this).find("span.title").text();
+      result.body = $(this).find("span.text").text();
+      result.img = $(this).find("img").attr("src");
+      result.link = $(this).find("a").attr("href");
 
-  // Select each element in the HTML body from which you want information.
-  // NOTE: Cheerio selectors function similarly to jQuery's selectors,
-  // but be sure to visit the package's npm page to see how it works
-  $("section.featuredArticle").each(function(i, element) {
-
-    var img = $(element).find("img").attr("src")
-    var title = $(element).find("span.title").text();
-    var body = $(element).find("span.text").text();
-    var link = $(element).find("a").attr("href");
-
-    // Save these results in an object that we'll push into the results array we defined earlier
-    results.push({
-      title: title,
-      body : body,
-      link: "https://www.premierleague.com" + link,
-      img: img
+      // Create a new Article using the `result` object built from scraping
+      db.Article.create(result)
+        .then(function(dbArticle) {
+          // View the added result in the console
+          console.log(dbArticle);
+        })
+        .catch(function(err) {
+          // If an error occurred, log it
+          console.log(err);
+        });
     });
-  });
 
-  // Log the results once you've looped through each of the elements found with cheerio
-  console.log(results);
+    // Message for completed scrape.
+    res.send("Scrape Complete");
+  });
 });
 
-// app.get("/scrape", function (req, res) {
+// Route for getting all Articles from the db
+app.get("/articles", function(req, res) {
+  // Grab every document in the Articles collection
+  db.Article.find({})
+    .then(function(dbArticle) {
+      // If we were able to successfully find Articles, send them back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
 
-//     axios.get("http://www.echojs.com/").then(function (response) {
+// Route for grabbing a specific Article by id, populate it with it's note
+app.get("/articles/:id", function(req, res) {
+  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+  db.Article.findOne({ _id: req.params.id })
+    // ..and populate all of the notes associated with it
+    .populate("note")
+    .then(function(dbArticle) {
+      // If we were able to successfully find an Article with the given id, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
 
-//         var $ = cheerio.load(response.date);
+// Route for saving/updating an Article's associated Note
+app.post("/articles/:id", function(req, res) {
+  // Create a new note and pass the req.body to the entry
+  db.Note.create(req.body)
+    .then(function(dbNote) {
+      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    })
+    .then(function(dbArticle) {
+      // If we were able to successfully update an Article, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
 
-
-//         $("article h2").each(function (i, element) {
-//             var result = {};
-
-//             result.title = $(this)
-//                 .children()
-//                 .text();
-//             // result.link = $(this)
-//             //     .find("a")
-//             //     .attr("href");
-
-//             db.Article.create(result)
-//               .then(function(dbArticle) {
-//                 console.log(dbArticle);
-//               })
-//               .catch(function(err) {
-//                 console.log(err);
-//               });
-//         });
-//         res.send("Scrape Complete");
-//     })
-// })
-
-app.listen(PORT, function () {
-    console.log("App running on port " + PORT)
-})
+// Start the server
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
